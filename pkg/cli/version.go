2c55797212b4bd31d8a1253c2016dbac17c357c3
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2025-04-02 19:16:07 +0900

    fix: fix compile errors

--- pkg/cli/version.go
+++ pkg/cli/version.go
@@ -1,6 +1,8 @@
 package cli
 
 import (
+	"context"
+
 	"github.com/urfave/cli/v3"
 )
 
@@ -14,7 +16,7 @@ func (vc *versionCommand) command() *cli.Command {
 	}
 }
 
-func (vc *versionCommand) action(c *cli.Context) error {
-	cli.ShowVersion(c)
+func (vc *versionCommand) action(_ context.Context, cmd *cli.Command) error {
+	cli.ShowVersion(cmd)
 	return nil
 }
